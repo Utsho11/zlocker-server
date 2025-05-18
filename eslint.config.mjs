90fbf7ecd13e8baf0f3a988526e0f6df5fa45eb5
Utsho11 <utshoroy521@gmail.com>
2025-05-19 03:34:47 +0600

    setup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    ignores: ["**/node_modules/", ".dist/"],
+    languageOptions: { globals: globals.node },
+    rules: {
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+      "no-undef": "error",
+    },
+  },
+  tseslint.configs.recommended,
+]);
