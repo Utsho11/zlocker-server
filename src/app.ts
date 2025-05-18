104370a5f838399644859b274fc10e0e765387f3
Utsho11 <utshoroy521@gmail.com>
2025-05-19 03:19:08 +0600

    initial setup

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import globalErrorHandler from "./app/middlewares/globalErrorhandler";
+import notFound from "./app/middlewares/notFound";
+// import router from './app/routes';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+
+// application routes
+// app.use('/api/v1', router);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hi Developer ! You are welcome to the server.");
+});
+
+app.use(globalErrorHandler);
+
+//Not Found
+app.use(notFound);
+
+export default app;
